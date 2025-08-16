80954b18f16c76b8d86d8a5d53ce54ca13f65984
abkr020 <abkr.020@gmail.com>
2025-08-16 22:12:14 +0530

    transform translate and box sizing and m and p to 0 and fixed positoin some good css

--- src/components/desktop/Desktop.tsx
+++ src/components/desktop/Desktop.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import FolderTab from '../folderTab/FolderTab'
 
 const Desktop = () => {
     return (
-        <div>
+        <div style={{backgroundColor:"black", color:"white",height:"100vh", width:"100vw", }}>
             desktop
+            <FolderTab />
         </div>
     )
 }
---
+++ src/components/folderTab/FolderTab.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const FolderTab = () => {
+  return (
+    <div style={{backgroundColor:"gray", color:"white", height:"50vh", width:"50vw",
+        position:"fixed", top:"50%", left:"50%",
+        transform:"translate(-50%, -50%)", 
+     }}>
+      foledre open 
+    </div>
+  )
+}
+
+export default FolderTab
